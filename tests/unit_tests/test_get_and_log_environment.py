764034b5475357a1a66ba8ca0399df1860b31326
Andy Worth <andyworth@flywheel.io>
2021-02-04 17:11:05 -0500

    rm pesky test when run in CI

--- tests/unit_tests/test_get_and_log_environment.py
+++ tests/unit_tests/test_get_and_log_environment.py
@@ -7,6 +7,10 @@ def test_get_and_log_environment_works(caplog, search_caplog):
 
     caplog.set_level(logging.DEBUG)
 
+    user_json = Path(Path.home() / ".config/flywheel/user.json")
+    if not user_json.exists():
+        TestCase.skipTest("", f"No API key available in {str(user_json)}")
+
     environ = get_and_log_environment()
     print(environ)
 
