dc663c8814dbe8f66303a5ba1d028efceb134cf3
Andy Worth <andyworth@flywheel.io>
2021-02-05 19:48:17 -0500

    add copy-job and end-to-end tests

---
+++ notebooks/copy-job.py
@@ -0,0 +1,123 @@
+#! /usr/bin/env python3
+#
+# Run bids-app-template on session "ses-Session2"
+#
+
+import argparse
+import os
+import pprint
+import stat
+from datetime import datetime
+
+import flywheel
+
+
+def main(job_id):
+
+    return_status = 0
+
+    fw = flywheel.Client("")
+    fw.get_config().site.api_url
+
+    job = fw.get_job(job_id)
+    gear = fw.get_gear(job.gear_id)
+    print(f"gear.gear.name is {gear.gear.name}")
+    destination_id = job.config["destination"]["id"]
+    print(f"job's analysis destination_id is {destination_id}")
+    analysis = fw.get_analysis(destination_id)
+    print(f"job's analysis's parent's type is {analysis.parent['type']}")
+    project_id = analysis.parents["project"]
+    project = fw.get_project(project_id)
+    project_label = project.label
+    print(f"Project label: {project.label}")
+    destination = fw.get(analysis.parent["id"])
+    print(f"job's destination is {destination.label}")
+    script_name = f"{project_label}_{analysis.parent['type']}_{destination.label}.py"
+    print(f"Creating script: {script_name} ...\n")
+
+    input_files = dict()
+    for key, val in job.config["inputs"].items():
+        file_name = val["location"]["name"]
+        input_files[key] = file_name
+
+    lines = f"""#! /usr/bin/env python3
+'''Run {gear.gear.name} on {analysis.parent['type']} "{destination.label}"'''
+
+import os
+import argparse
+from datetime import datetime
+
+
+import flywheel
+
+
+def main():
+
+    fw = flywheel.Client('')
+    print(fw.get_config().site.api_url)
+
+    gear = fw.lookup("gears/{gear.gear.name}")
+    print("gear.gear.version for job was = {gear.gear.version}")"""
+
+    with open(script_name, "w") as sfp:
+        for line in lines.split("\n"):
+            sfp.write(line + "\n")
+        sfp.write('    print(f"gear.gear.version now = {gear.gear.version}")\n')
+        sfp.write(f"    print(\"destination_id = {analysis.parent['id']}\")\n")
+        sfp.write(f"    print(\"destination type is: {analysis.parent['type']}\")\n")
+        sfp.write(f"    destination = fw.get(\"{analysis.parent['id']}\")\n")
+        sfp.write("\n")
+        sfp.write(f'    project = fw.get_project("{project_id}")\n')
+        sfp.write("    inputs = dict()\n")
+        sfp.write(f"    for key, val in {input_files}.items():\n")
+        sfp.write("        inputs[key] = project.get_file(val)\n")
+        sfp.write("\n")
+        sfp.write(f"    config = {pprint.pformat(job['config']['config'], indent=4)}\n")
+        sfp.write("\n")
+        sfp.write("    now = datetime.now()\n")
+        sfp.write("    analysis_label = (\n")
+        sfp.write(
+            "        f'{gear.gear.name} {now.strftime(\"%m-%d-%Y %H:%M:%S\")} SDK launched'\n"
+        )
+        sfp.write("    )\n")
+        sfp.write('    print(f"analysis_label = {analysis_label}")\n')
+        sfp.write("\n")
+        sfp.write("    analysis_id = gear.run(\n")
+        sfp.write("        analysis_label=analysis_label,\n")
+        sfp.write("        config=config,\n")
+        sfp.write("        inputs=inputs,\n")
+        sfp.write("        destination=destination,\n")
+        sfp.write("    )\n")
+        sfp.write("    return analysis_id\n")
+        sfp.write("\n")
+        sfp.write("\n")
+        sfp.write("if __name__ == '__main__':\n")
+        sfp.write("\n")
+        sfp.write("    parser = argparse.ArgumentParser(description=__doc__)\n")
+        sfp.write("    args = parser.parse_args()\n")
+        sfp.write("\n")
+        sfp.write("    analysis_id = main()\n")
+        sfp.write("\n")
+        sfp.write('    print(f"analysis_id = {analysis_id}")\n')
+        sfp.write("\n")
+        sfp.write("    os.sys.exit(0)\n")
+
+    os.system(f"black {script_name}")
+
+    st = os.stat(script_name)
+    os.chmod(script_name, st.st_mode | stat.S_IEXEC)
+
+    return return_status
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("job_id", help="Flywheel job ID")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+
+    args = parser.parse_args()
+
+    os.sys.exit(main(args.job_id))
---
+++ tests/end-to-end-tests/BIDS_multi_session_session_ses-Session2_dry-run-fails.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+'''Run bids-app-template on session "ses-Session2"'''
+
+import argparse
+import os
+from datetime import datetime
+
+import flywheel
+
+
+def main():
+
+    fw = flywheel.Client("")
+    print(fw.get_config().site.api_url)
+
+    gear = fw.lookup("gears/bids-app-template")
+    print("gear.gear.name = bids-app-template")
+    print("gear.gear.version = 0.0.0_0.14.0")
+
+    destination_id = "5dc091f669d4f3002a16ee9d"
+    print("destination id is: 601c74110533ee34c0708d3d")
+    print("destination type is: session")
+    destination = fw.get(destination_id)
+
+    inputs = {}
+
+    config = {
+        "bids_app_args": "",
+        "example-bool-param": False,
+        "example-empty-param": "",
+        "example-threshold": 3.1415926,
+        "gear-dry-run": True,
+        "gear-ignore-bids-errors": False,
+        "gear-intermediate-files": "",
+        "gear-intermediate-folders": "",
+        "gear-keep-output": False,
+        "gear-log-level": "DEBUG",
+        "gear-run-bids-validation": True,
+        "gear-save-intermediate-output": False,
+        "ignore": "",
+        "verbose": "v",
+        "write-graph": False,
+    }
+
+    now = datetime.now()
+    analysis_label = f'{gear.gear.name} {now.strftime("%m-%d-%Y %H:%M:%S")} BIDS error'
+    print(f"analysis_label = {analysis_label}")
+
+    analysis_id = gear.run(
+        analysis_label=analysis_label,
+        config=config,
+        inputs=inputs,
+        destination=destination,
+    )
+    return analysis_id
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    args = parser.parse_args()
+
+    analysis_id = main()
+
+    print(f"analysis_id = {analysis_id}")
+
+    os.sys.exit(0)
