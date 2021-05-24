0478d74eaaac432c28a7a1b6649ca8efeb9f9363
Andy Worth <andyworth@flywheel.io>
2021-05-24 19:44:46 -0400

    put skip test in test

--- tests/unit_tests/test_environment.py
+++ tests/unit_tests/test_environment.py
@@ -1,4 +1,6 @@
 import logging
+from pathlib import Path
+from unittest import TestCase
 
 from run import get_and_log_environment
 
@@ -6,6 +8,11 @@ log = logging.getLogger(__name__)
 
 
 def test_get_and_log_environment_works(caplog, search_caplog):
+
+    # Skip this test in GitHub CI because it works locally and for real runs but not CI
+    user_json = Path(Path.home() / ".config/flywheel/user.json")
+    if not user_json.exists():
+        TestCase.skipTest("", f"No API key available in {str(user_json)}")
 
     caplog.set_level(logging.DEBUG)
 
